7d1aede196c88c8fa95f8272719e28bd6b106797
Andrii Holoshivskyi <andrii.holoshivskyi@gmail.com>
2021-12-22 17:17:37 +0200

    minifix

--- src/main/java/com/example/demolibrary/DemoLibraryApplication.java
+++ src/main/java/com/example/demolibrary/DemoLibraryApplication.java
@@ -1,6 +1,5 @@
 package com.example.demolibrary;
 
-import com.github.messenger4j.Messenger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +7,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoLibraryApplication {
-
-    @Bean
-    public Messenger messenger(@Value("${messenger4j.pageAccessToken}") String pageAccessToken,
-                               @Value("${messenger4j.appSecret}") final String appSecret,
-                               @Value("${messenger4j.verifyToken}") final String verifyToken) {
-        return Messenger.create(pageAccessToken, appSecret, verifyToken);
-    }
 
     @Bean
     public MyMessenger myMessenger(@Value("${messenger4j.pageAccessToken}") String pageAccessToken,
