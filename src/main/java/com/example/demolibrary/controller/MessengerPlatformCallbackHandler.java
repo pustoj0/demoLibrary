00c17f7079c1c76be013c73d5ebf45fb4f2d5fb9
Andrii Holoshivskyi <andrii.holoshivskyi@gmail.com>
2021-12-28 11:21:27 +0200

    test #35

--- src/main/java/com/example/demolibrary/controller/MessengerPlatformCallbackHandler.java
+++ src/main/java/com/example/demolibrary/controller/MessengerPlatformCallbackHandler.java
@@ -54,7 +54,7 @@ public class MessengerPlatformCallbackHandler {
     public ResponseEntity<String> handleCallback(@RequestBody MessagePayloadDTO messagePayloadDTO,
                                                  @RequestHeader(SIGNATURE_HEADER_NAME) final String signature) {
         logger.info("Received Messenger Platform callback - payload: {} | signature: {}", messagePayloadDTO, signature);
-        sendGenericTemplateService.sendMessage(messagePayloadDTO);
+        //sendGenericTemplateService.sendMessage(messagePayloadDTO);
         return ResponseEntity.ok("Message was sent");
     }
 }
